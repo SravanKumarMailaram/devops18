14c65291686f5aed0d5a175310c54d376cb381a9
SravanKumarMailaram <mailaramsravan.143.ms@gmail.com>
2025-11-18 18:11:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-0c7d68785ec07306c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "AwsKeyPair"
+    key_name = "AwsKeyPairs"
     tags = {
         Name = "DevOps"
     }
